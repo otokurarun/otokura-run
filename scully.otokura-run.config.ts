37480eae20ec82d267c4218183a4f4f5b33a54f7
rairus00 <121190401+rairus00@users.noreply.github.com>
2023-02-04 19:22:16 +0900

    Feat: esa記事の画像が表示されるように変更

--- scully.otokura-run.config.ts
+++ scully.otokura-run.config.ts
@@ -3,6 +3,8 @@ import { ScullyConfig } from '@scullyio/scully';
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer'
 
+import 'scully-plugin-esa';
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "otokura-run",
