dbfe1aa0a5855af2022d9de6f1d80577d8968c3b
rairus00 <121190401+rairus00@users.noreply.github.com>
2023-02-04 19:49:43 +0900

    blogコンポーネントの削除(再)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,12 @@ import { MaterialModule } from 'src/material.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { ArticlesComponent } from './blog/articles/articles.component';
-import { BlogComponent } from './blog/blog.component';
 import { ArticleDetailComponent } from './blog/article-detail/article-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    BlogComponent,
     ArticlesComponent,
     ArticleDetailComponent
   ],
